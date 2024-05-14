99eff64ed501d2e12458964c7f0124729ee40a8e
Barion <ametrin.studios@gmail.com>
2024-05-14 21:14:26 +0200

    DependencyObjectExtensions cleanup

--- DependencyObjectExtensions.cs
+++ DependencyObjectExtensions.cs
@@ -1,42 +1,39 @@
-﻿using System.Windows.Media;
+﻿using Ametrin.Utils;
+using System.Windows.Media;
 using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ametrin.Utils.WPF;
 
 public static class DependencyObjectExtensions {
-    public static DependencyObject? FindParent(this DependencyObject child) {
-        return VisualTreeHelper.GetParent(child);
+    public static DependencyObject? GetParent(this DependencyObject child) => VisualTreeHelper.GetParent(child);
+    public static T? FindParentOrSelf<T>(this DependencyObject obj) where T : DependencyObject => obj is T typed ? typed : obj.FindParent<T>();
+    public static T? FindParent<T>(this DependencyObject obj) where T : DependencyObject {
+        return VisualTreeHelper.GetParent(obj) switch {
+            null => null,
+            T typed => typed,
+            var parent => parent.FindParent<T>(),
+        };
     }
 
-    public static T? FindParent<T>(this DependencyObject child) where T : DependencyObject {
-        var parent = VisualTreeHelper.GetParent(child);
 
-        if(parent is null) return null;
-
-        if(parent is T) return parent as T;
-
-        return FindParent<T>(parent);
+    public static DependencyObject? GetChild(this DependencyObject child, int index) => VisualTreeHelper.GetChild(child, index);
+    public static T? FindChildOrSelf<T>(this DependencyObject obj) where T : DependencyObject => obj is T typed ? typed : obj.FindChild<T>();
+    public static T? FindChild<T>(this DependencyObject obj) where T : DependencyObject {
+        var children = obj.EnumerateChildren();
+        return children.OfType<T>().FirstOrDefault() ?? children.Select(FindChild<T>).FirstOrDefault(c => c is not null);
     }
-
-    public static T? FindChild<T>(this DependencyObject parent) where T : DependencyObject {
-        for(var i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
-            var child = VisualTreeHelper.GetChild(parent, i);
-            if(child is T t) {
-                return t;
+    public static IEnumerable<DependencyObject> EnumerateChildren(this DependencyObject obj) {
+        foreach(var index in ..VisualTreeHelper.GetChildrenCount(obj)) {
+            if(obj.GetChild(index) is DependencyObject child) {
+                yield return child;
             }
         }
-
-        for(var i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
-            var child = VisualTreeHelper.GetChild(parent, i);
-            var childOfChild = child.FindChild<T>();
-            if(childOfChild is not null) {
-                return childOfChild;
-            }
-        }
-
-        return null;
     }
 
+    [Obsolete] // use find parent or self
     public static T FindAncestorOrSelf<T>(this DependencyObject obj) where T : DependencyObject {
         while(obj is not null) {
             if(obj is T objTyped)
