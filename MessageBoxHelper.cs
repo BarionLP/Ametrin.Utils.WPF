f27c04ba4c8fe06e7dbb0abdbf2494dc4acb6ab5
BarionLP <ametrin.studios@gmail.com>
2024-05-21 15:10:13 +0200

    added MessageBoxHelper

---
+++ MessageBoxHelper.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Ametrin.Utils.WPF;
+
+public static class MessageBoxHelper {
+    public static MessageBoxResult ShowError(string error, string caption = "Error") 
+        => MessageBox.Show(error, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+}
