1550da2cbe2647d32643a2f50eaf3ece8430ab92
BarionLP <ametrin.studios@gmail.com>
2024-06-14 11:24:54 +0200

    added owner to MessageBoxHelper.ShowSuccess

--- MessageBoxHelper.cs
+++ MessageBoxHelper.cs
@@ -6,6 +6,8 @@ public static class MessageBoxHelper {
     public static MessageBoxResult ShowError(string error, string caption = "Error") 
         => MessageBox.Show(error, caption, MessageBoxButton.OK, MessageBoxImage.Error);
     
-    public static MessageBoxResult ShowSuccess(string error, string caption = "Success") 
-        => MessageBox.Show(error, caption, MessageBoxButton.OK, MessageBoxImage.Information);
+    public static MessageBoxResult ShowSuccess(string error, string caption = "Success", Window? owner = null) 
+        => owner is null 
+            ? MessageBox.Show(error, caption, MessageBoxButton.OK, MessageBoxImage.Information) 
+            : MessageBox.Show(owner, error, caption, MessageBoxButton.OK, MessageBoxImage.Information);
 }
